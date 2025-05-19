21e5eb867b80f1794587ea6af3387d42ec435b28
marostica <joao.roberto.marostica@gmail.com>
2025-05-19 13:54:07 -0400

    deletei uma biblioteca

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,28 +1,8 @@
-use calamine::{open_workbook, Reader, Xlsx};
-
-#[tauri::command]
-fn process_file(path: &str) -> Option<String> {
-    let mut wb: Xlsx<_> = open_workbook(path).expect("Impossível abrir arquivo");
-    let range = wb.worksheet_range_at(0);
-    match range {
-        Some(res) => {
-            if let Ok(_range) = res {
-                print!("funciona\n");
-                None
-            } else {
-                Some("não foi possível abrir a planilha 1\n".to_string())
-            }
-        },
-        None => Some("não foi possível abrir a planilha 2\n".to_string())
-    }
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            process_file
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- src-tauri/tests/test.rs
+++ src-tauri/tests/test.rs
@@ -1,10 +1,4 @@
 #[cfg(test)]
 mod tests {
-    /*use seed_manager_lib::process_file;
-
-    #[test]
-    fn process_file_test() {
-        process_file("./tests/planilha.xlsx");
-    }*/
 
 }
