5691d8b390a1163c01097753586da472fb7b6518
marostica <joao.roberto.marostica@gmail.com>
2025-05-28 06:11:59 -0400

    batch repo done

--- src-tauri/src/models/batch.rs
+++ src-tauri/src/models/batch.rs
@@ -1,18 +1,49 @@
-use diesel::prelude::Queryable;
-use serde::Serialize;
+use diesel::prelude::{AsChangeset, Insertable, Queryable, Selectable};
 
-use crate::models::{balance::Balance, seed::Seed, coating::Coating, brand::Brand};
-
-#[derive(Queryable,Serialize)]
+#[derive(Queryable,Selectable,Insertable,AsChangeset)]
+#[diesel(table_name = crate::schema::tb_batch)]
 pub struct Batch {
-    batch_number:u32,
-    year:u32,
-    month:u32,
-    seed:Seed,
-    coating:Coating,
-    brand:Brand,
-    sack_weight:u32,
+    batch_number:i32,
+    batch_year:i32,
+    batch_month:i32,
+    seed:String,
+    coating:String,
+    brand:String,
+    sack_weight:i32,
+    sack_amount: i32,
+    total_weight: i32,
     pureness_score:f32,
-    initial_balance:Balance,
+    total_pureness_score: f32,
     origin:String
+}
+
+impl Batch {
+    pub fn new(
+        batch_number:i32,
+        batch_year:i32,
+        batch_month:i32,
+        seed:String,
+        coating:String,
+        brand:String,
+        sack_weight:i32,
+        sack_amount:i32,
+        total_weight:i32,
+        pureness_score:f32,
+        total_pureness_score:f32,
+        origin:String) -> Self {
+            Batch {
+                batch_number:batch_number,
+                batch_year:batch_year,
+                batch_month:batch_month,
+                seed:seed,
+                coating:coating,
+                brand:brand,
+                sack_weight:sack_weight,
+                sack_amount:sack_amount,
+                total_weight:total_weight,
+                pureness_score:pureness_score,
+                total_pureness_score:total_pureness_score,
+                origin:origin
+            }
+        }
 }
--- src-tauri/src/repositories/batch_repository.rs
+++ src-tauri/src/repositories/batch_repository.rs
@@ -1,25 +1,79 @@
+use diesel::{prelude::*,SqliteConnection};
+use dotenvy::dotenv;
+
+use core::panic;
+use std::env;
+
+use crate::schema::tb_batch::dsl::*;
 use crate::models::batch::Batch;
 
-pub struct BatchRepository;
+pub struct BatchRepository {
+    connection:SqliteConnection
+}
 
 impl BatchRepository {
-    fn create(object:&Batch) {
+    pub fn new() -> Self {
+        dotenv().ok();
 
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+        let connection = SqliteConnection::establish(&database_url).unwrap_or_else(|_| panic!("Error connecting to {}", database_url));
+        Self { connection: connection }
     }
 
-    fn read(id:&(u32,u32)) -> Option<Batch> {
-        None
+    pub fn create(&mut self, object:&Batch) -> Batch {
+        diesel::insert_into(tb_batch)
+        .values(object)
+        .returning(Batch::as_returning())
+        .get_result(&mut self.connection)
+        .expect("Error creating a new record in batch table")
     }
 
-    fn read_all() -> Vec<Batch> {
-        vec![]
+    pub fn read(&mut self, id:&(i32,i32)) -> Option<Batch> {
+        let (bn, by) = id;
+        match tb_batch
+        .filter(
+            batch_number.eq(bn)
+            .and(batch_year.eq(by)))
+        .select(Batch::as_select())
+        .get_result(&mut self.connection)
+        .optional() {
+            Ok(option) => option,
+            Err(e) => panic!("Error trying to read batch id={:?}\n{}", id, e)
+        }
+ 
     }
 
-    fn update(id:&(u32,u32), object:&Batch) {
-
+    pub fn read_all(&mut self) -> Vec<Batch> {
+        match tb_batch
+        .select(Batch::as_select())
+        .get_results(&mut self.connection) {
+            Ok(result) => result,
+            Err(e) => panic!("Error trying to read all batchs\n{}", e)
+        }
     }
 
-    fn delete(id:&(u32,u32)) {
+    pub fn update(&mut self, id:&(i32, i32), object:&Batch) -> Option<Batch> {
+        let (bn, by) = id;
+        match diesel::update(
+            tb_batch.filter(
+                batch_number.eq(bn)
+            .and(batch_year.eq(by))))
+        .set(object)
+        .returning(Batch::as_returning())
+        .get_result(&mut self.connection)
+        .optional() {
+            Ok(option) => option,
+            Err(e) => panic!("Error trying to update batch id={:?}\n{}", id, e)
+        }
+    }
 
+    pub fn delete(&mut self, id:&(i32,i32)) -> usize {
+        let (bn, by) = id;
+        diesel::delete(
+            tb_batch.filter(
+                batch_number.eq(bn)
+            .and(batch_year.eq(by))))
+        .execute(&mut self.connection)
+        .expect("Error deleting record in batch table")
     }
 }
