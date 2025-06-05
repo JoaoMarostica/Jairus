d3b9ec68d71311b87602824a5943b5d8f3dffad1
cck <santosguilherme783@gmail.com>
2025-06-05 11:17:44 -0400

    added brands and coatings stores and updated seedsStore

---
+++ src/stores/brandsStore.ts
@@ -0,0 +1,142 @@
+import { defineStore } from 'pinia';
+import type { BrandDB, DataTableBrand} from '@/types/brands';
+import { invoke } from '@tauri-apps/api/core';
+
+
+
+export const useBrandsStore = defineStore('brands', {
+  state: () => ({
+    brands: [] as BrandDB[],
+    dataTableBrands: [] as DataTableBrand[],
+  }),
+  actions: {
+    async fetchSeeds() {
+        try {
+            this.$reset;
+
+            this.brands = await invoke('list_brands');
+
+            this.brands.forEach((brand: BrandDB) =>
+                this.dataTableBrands.push(formatBrandForTable(brand))
+            );
+        } catch (err) {
+            console.error(err);
+        }
+    },
+    
+    async createBrand(newBrand: BrandDB) {
+        try {
+            const createdBrand: BrandDB = await invoke('add_brand', {
+                brand: newBrand
+            });
+
+           this.dataTableBrands.push(formatBrandForTable(createdBrand));
+        } catch (err) {
+            console.error('Erro ao criar Marca:', err);
+        }
+    },
+
+    async editBrand(brand: BrandDB) {
+        try {
+            const editedBrand: BrandDB = await invoke('change_brand', {
+                brandName: brand.brand_name,
+                weights: brand.weights
+            });
+
+            const index = this.dataTableBrands.findIndex(b => b.key === brand.brand_name);
+
+            if (index !== -1) {
+                this.dataTableBrands[index] = formatBrandForTable(editedBrand);
+            }
+
+            this.brands = await invoke('list_brands');
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    },
+
+    async removeBrand(brand: DataTableBrand) {
+        try {
+            await invoke('remove_brand', {
+                brandName: brand.brand_name
+            });
+
+            await this.fetchSeeds();
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    },
+    async addBrandWeight(brandName: string, weight: number) {
+        try{
+            const brandIndex = this.brands.findIndex(b => b.brand_name === brandName);
+            if (brandIndex === -1){
+                console.error(`Marca ${brandName} não encontrada`);
+
+            }
+
+            if( this.brands[brandIndex].weights.includes(weight)){
+               console.error(`Peso ${weight}Kg já existe para a marca ${brandName}`);
+            }
+
+            await invoke('add_brand_weight', {
+                brandName: brandName,
+                weight: weight
+            });
+
+            this.brands[brandIndex].weights.push(weight);
+            const tableIndex = this.dataTableBrands.findIndex(b => b.key === brandName);
+            if (tableIndex !== -1) {
+                this.dataTableBrands[tableIndex] = formatBrandForTable(this.brands[brandIndex]);
+            }
+        } catch (err) {
+            console.error('Erro ao adicionar peso ', err);
+            throw err;
+        }
+    },
+    async removeBrandWeight(brandName: string, weight: number) {
+        try{
+            const brandIndex = this.brands.findIndex(b => b.brand_name === brandName);
+            if (brandIndex === -1){
+                console.error(`Marca ${brandName} não encontrada`);
+
+            }
+
+            if(!this.brands[brandIndex].weights.includes(weight)){
+                console.error(`Peso ${weight}Kg não existe para a marca ${brandName}`);
+            }
+
+            await invoke('remove_brand_weight', {
+                brandName: brandName,
+                weight: weight
+            });
+
+            this.brands[brandIndex].weights = this.brands[brandIndex].weights.filter(w => w !== weight);
+            const tableIndex = this.dataTableBrands.findIndex(b => b.key === brandName);
+            if (tableIndex !== -1) {
+                this.dataTableBrands[tableIndex] = formatBrandForTable(this.brands[brandIndex]);
+            }
+        } catch (err) {
+            console.error('Erro ao remover peso ', err);
+            throw err;
+        }
+    },
+
+    
+    
+}});
+
+function formatBrandForTable(brand: BrandDB): DataTableBrand {
+    const brandForTable: DataTableBrand = {
+        key: brand.brand_name,
+        brand_name: brand.brand_name,
+        weights: brand.weights.map(weight => ({
+            label: weight.toString() + 'Kg',
+            value: weight.toString()
+        }))
+   
+    };
+
+    return brandForTable
+}
---
+++ src/stores/coatingsStore.ts
@@ -0,0 +1,84 @@
+import { defineStore } from 'pinia';
+import type { CoatingDB, DataTableCoating } from '@/types/coatings';
+import { invoke } from '@tauri-apps/api/core';
+
+
+export const useCoatingsStore = defineStore('coatings', {
+  state: () => ({
+    coatings: [] as CoatingDB[],
+    dataTableCoatings: [] as DataTableCoating[],
+  }),
+  actions: {
+    async fetchCoatings() {
+        try {
+            this.$reset;
+
+            this.coatings = await invoke('list_coatings');
+
+            this.coatings.forEach((coating: CoatingDB) =>
+                this.dataTableCoatings.push(formatCoatingForTable(coating))
+            );
+        } catch (err) {
+            console.error(err);
+        }
+    },
+
+    async createCoating(newCoating: CoatingDB) {
+        try {
+            const createdCoating: CoatingDB = await invoke('add_coating', {
+                seed: newCoating
+            });
+
+           this.dataTableCoatings.push(formatCoatingForTable(createdCoating));
+        } catch (err) {
+            console.error('Erro ao criar Tratamento:', err);
+        }
+    },
+
+    async editCoating(coating: CoatingDB) {
+        try {
+            const editedCoating: CoatingDB = await invoke('change_coating', {
+                coatingName: coating.coating_name
+            });
+    
+            
+            const index = this.dataTableCoatings.findIndex(c => c.key === coating.coating_name);
+
+
+            if (index !== -1) {
+                this.dataTableCoatings[index] = formatCoatingForTable(editedCoating);
+            }
+    
+            this.coatings = await invoke('list_coatings');
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    },
+
+    async removeCoating(coating: DataTableCoating) {
+        try {
+            await invoke('remove_coating', {
+                coatingName: coating.coating_name
+                    
+            });
+    
+            await this.fetchCoatings();
+        } catch (err) {
+            console.error(err);
+            throw err;
+        }
+    },
+  },
+ 
+});
+
+function formatCoatingForTable(coating: CoatingDB): DataTableCoating {
+    const seedForTable: DataTableCoating = {
+        key: coating.coating_name,
+        coating_name: coating.coating_name,
+        
+    };
+
+    return seedForTable
+}
