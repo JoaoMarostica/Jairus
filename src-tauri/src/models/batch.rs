6f5a4187dac83f498ef7d1567ece5906ed672ed5
marostica <joao.roberto.marostica@gmail.com>
2025-06-05 18:37:21 -0400

    formating

--- src-tauri/src/models/batch.rs
+++ src-tauri/src/models/batch.rs
@@ -12,20 +12,20 @@ use serde::{
 #[derive(Queryable, Selectable, Insertable, AsChangeset, Deserialize, Serialize)]
 #[diesel(table_name = crate::schema::tb_batch)]
 pub struct Batch {
-    pub batch_number:i32,
-    pub batch_year:i32,
-    pub batch_month:i32,
-    pub seed:String,
-    pub coating:String,
-    pub brand:String,
-    pub sack_weight:i32,
-    pub sack_amount:i32,
-    pub total_weight:i32,
-    pub pureness_score:f32,
-    pub total_pureness_score:f32,
-    pub batch_status:i32,
-    pub deleted_at:Option<String>,
-    pub origin:Option<String>
+    pub batch_number: i32,
+    pub batch_year: i32,
+    pub batch_month: i32,
+    pub seed: String,
+    pub coating: String,
+    pub brand: String,
+    pub sack_weight: i32,
+    pub sack_amount: i32,
+    pub total_weight: i32,
+    pub pureness_score: f32,
+    pub total_pureness_score: f32,
+    pub batch_status: i32,
+    pub deleted_at: Option<String>,
+    pub origin: Option<String>
 }
 
 impl Batch {
@@ -41,7 +41,7 @@ impl Batch {
         total_weight: i32,
         pureness_score: f32,
         total_pureness_score: f32,
-        batch_status:i32) -> Self {
+        batch_status: i32) -> Self {
             Batch {
                 batch_number,
                 batch_year,
