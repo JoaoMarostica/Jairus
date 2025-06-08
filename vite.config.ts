1c74cd4dfa244524f128e439ddd19971ba1833a6
SamuelKrabbe <krabbe.samuel@gmail.com>
2025-06-08 14:14:35 -0400

    changing configs

--- vite.config.ts
+++ vite.config.ts
@@ -7,8 +7,8 @@ import { NaiveUiResolver } from 'unplugin-vue-components/resolvers';
 
 const host = process.env.TAURI_DEV_HOST;
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [vue(), 
     AutoImport({
       resolvers: [NaiveUiResolver()],
